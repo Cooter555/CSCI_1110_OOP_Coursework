34be1396bff241d02d66908bf62509f67076ab34
Cooter555 <42577104+Cooter555@users.noreply.github.com>
2020-10-01 07:12:40 -0600

    Add files via upload

---
+++ Chapter21/statesAndCapitals (1).java
@@ -0,0 +1,80 @@
+//Cooter Gale
+import java.util.*;
+
+public class statesAndCapitals {
+  public static void main(String[] args) {	
+    Scanner input = new Scanner(System.in);
+    Map<String, String> statesAndCapitals = getData();
+      System.out.print("Enter state: ");
+      String state = input.nextLine();
+      
+    if (statesAndCapitals.get(state) != null) {
+      System.out.println("You seriously don't know the capital of " + state + "?");
+      System.out.println("Well if you have to know, the capital of " + state + " is " + statesAndCapitals.get(state));
+    }
+    else System.out.println(state + " isn't a state you moron.");
+  }
+  
+public static Map<String, String> getData() {
+  Map<String, String> map = new HashMap<>();
+    String[][] stateCapital = {
+      {"Alabama", "Montgomery"},
+      {"Alaska", "Juneau"},
+      {"Arizona", "Phoenix"},
+      {"Arkansas", "Little Rock"},
+      {"California", "Sacramento"},
+      {"Colorado", "Denver"},
+      {"Connecticut", "Hartford"},
+      {"Delaware", "Dover"},
+      {"Florida", "Tallahassee"},
+      {"Georgia", "Atlanta"},
+      {"Hawaii", "Honolulu"},
+      {"Idaho", "Boise"},
+      {"Illinois", "Springfield"},
+      {"Indiana", "Indianapolis"},
+      {"Iowa", "Des Moines"},
+      {"Kansas", "Topeka"},
+      {"Kentucky", "Frankfort"},
+      {"Louisiana", "Baton Rouge"},
+      {"Maine", "Augusta"},
+      {"Maryland", "Annapolis"},
+      {"Massachusettes", "Boston"},
+      {"Michigan", "Lansing"},
+      {"Minnesota", "Saint Paul"},
+      {"Mississippi", "Jackson"},
+      {"Missouri", "Jefferson City"},
+      {"Montana", "Helena"},
+      {"Nebraska", "Lincoln"},
+      {"Nevada", "Carson City"},
+      {"New Hampshire", "Concord"},
+      {"New Jersey", "Trenton"},
+      {"New York", "Albany"},
+      {"New Mexico", "Santa Fe"},
+      {"North Carolina", "Raleigh"},
+      {"North Dakota", "Bismarck"},
+      {"Ohio", "Columbus"},
+      {"Oklahoma", "Oklahoma City"},
+      {"Oregon", "Salem"},
+      {"Pennsylvania", "Harrisburg"},
+      {"Rhode Island", "Providence"},
+      {"South Carolina", "Columbia"},
+      {"South Dakota", "Pierre"},
+      {"Tennessee", "Nashville"},
+      {"Texas", "Austin"},
+      {"Utah", "Salt Lake City"},
+      {"Vermont", "Montpelier"},
+      {"Virginia", "Richmond"},
+      {"Washington", "Olympia"},
+      {"West Virginia", "Charleston"},
+      {"Wisconsin", "Madison"},
+      {"Wyoming", "Cheyenne"}
+    };
+
+    for (int i = 0; i < stateCapital.length; i++) {
+          map.put(stateCapital[i][0], stateCapital[i][1]);
+        }
+
+        return map;
+   
+  }
+}
