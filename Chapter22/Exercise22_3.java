58ec2d77111768c2e5629d66a7ab39bfe7900872
Cooter555 <42577104+Cooter555@users.noreply.github.com>
2020-10-13 07:37:54 -0600

    Add files via upload

---
+++ Chapter22/Exercise22_3.java
@@ -0,0 +1,39 @@
+import java.util.*;
+// 1 simple loop
+// 4 simple satements
+// T(n) = 4c * n;
+class Exercise22_3 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter s1: ");
+		String s1 = input.nextLine();
+		System.out.println("Enter s2: ");
+		String s2 = input.nextLine();
+		
+		int index = -1;
+		int count = 0;
+		
+		for(int i = 0; i < s1.length(); i++) {
+			if (s1.charAt(i) == s2.charAt(0) && count == 0) {
+				index = i;
+				count++;
+			}
+			else if ((s1.charAt(i) == s2.charAt(count))) {
+				count++;
+			}
+			else {
+				count = 0;
+				index = -1;
+			}
+			if (count == s2.length())
+				break;
+		}
+		if (index > 1) {
+			System.out.println("Matched at index: " + index);
+		}
+		else {
+			System.out.println("no match");
+		}
+	}
+}
